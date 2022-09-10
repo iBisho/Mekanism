a9afa8fd6664867c71be677ef8e0856c52bd3166
Bisho <37049432+iBisho@users.noreply.github.com>
2022-09-09 21:47:20 -0300

    Update TileEntityChemicalWasher.java

--- src/main/java/mekanism/common/tile/machine/TileEntityChemicalWasher.java
+++ src/main/java/mekanism/common/tile/machine/TileEntityChemicalWasher.java
@@ -198,7 +198,7 @@ public class TileEntityChemicalWasher extends TileEntityRecipeMachine<FluidSlurr
     public void recalculateUpgrades(Upgrade upgrade) {
         super.recalculateUpgrades(upgrade);
         if (upgrade == Upgrade.SPEED) {
-            baselineMaxOperations = (int) Math.pow(2, upgradeComponent.getUpgrades(Upgrade.SPEED) + 1);
+            baselineMaxOperations = (int) Math.pow(2.4, upgradeComponent.getUpgrades(Upgrade.SPEED));
         }
     }
 
